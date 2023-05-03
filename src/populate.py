59c540598d3c73c2f1a52b152fd59679b03c5021
Servet <serveth@gmail.com>
2023-05-03 21:56:11 +0200

    Sample data to populate

--- src/populate.py
+++ src/populate.py
@@ -1,50 +1,31 @@
-lecturers = [
-    {'title': 'Dr.', 'name': 'John', 'surname': 'Smith'},
-    {'title': 'Prof.', 'name': 'Jane', 'surname': 'Doe'},
-    {'title': 'Dr.', 'name': 'Michael', 'surname': 'Johnson'},
-    {'title': 'Prof.', 'name': 'Sarah', 'surname': 'Lee'}
-]
+import sqlite3
+from pathlib import Path
 
-courses_dict = [
-    {'course_code': 'CS101', 'course_name': 'Introduction to CS'},
-    {'course_code': 'CS102', 'course_name': 'Data Structures and Algos'},
-    {'course_code': 'CS103', 'course_name': 'Computer Networks'},
-    {'course_code': 'CS104', 'course_name': 'Operating Systems'},
-    {'course_code': 'CS105', 'course_name': 'Artificial Intelligence'}
-]
 
-students = [
-    {'student_id': 1, 'name': 'John', 'surname': 'Doe'},
-    {'student_id': 2, 'name': 'Jane', 'surname': 'Smith'},
-    {'student_id': 3, 'name': 'Bob', 'surname': 'Johnson'},
-    {'student_id': 4, 'name': 'Mary', 'surname': 'Brown'},
-    {'student_id': 5, 'name': 'Jack', 'surname': 'Davis'}
-]
+class Populate:
+    def __init__(self) -> None:
+        # Load configurations
+        self.database = 'database/university.db'
+        self.sql_command = Path('database/commands/sample_data.sql').read_text()
 
-grades = [
-    {'student_id': 1, 'course_code': 'CS101', 'grade': 4.0},
-    {'student_id': 1, 'course_code': 'CS102', 'grade': 3.0},
-    {'student_id': 1, 'course_code': 'CS103', 'grade': 2.0},
-    {'student_id': 1, 'course_code': 'CS104', 'grade': 1.0},
-    {'student_id': 1, 'course_code': 'CS105', 'grade': 5.0},
-    {'student_id': 2, 'course_code': 'CS101', 'grade': 2.0},
-    {'student_id': 2, 'course_code': 'CS102', 'grade': 1.0},
-    {'student_id': 2, 'course_code': 'CS103', 'grade': 5.0},
-    {'student_id': 2, 'course_code': 'CS104', 'grade': 4.0},
-    {'student_id': 2, 'course_code': 'CS105', 'grade': 3.0},
-    {'student_id': 3, 'course_code': 'CS101', 'grade': 3.0},
-    {'student_id': 3, 'course_code': 'CS102', 'grade': 2.0},
-    {'student_id': 3, 'course_code': 'CS103', 'grade': 1.0},
-    {'student_id': 3, 'course_code': 'CS104', 'grade': 5.0},
-    {'student_id': 3, 'course_code': 'CS105', 'grade': 4.0},
-    {'student_id': 4, 'course_code': 'CS101', 'grade': 1.0},
-    {'student_id': 4, 'course_code': 'CS102', 'grade': 2.0},
-    {'student_id': 4, 'course_code': 'CS103', 'grade': 5.0},
-    {'student_id': 4, 'course_code': 'CS104', 'grade': 4.0},
-    {'student_id': 4, 'course_code': 'CS105', 'grade': 3.0},
-    {'student_id': 5, 'course_code': 'CS101', 'grade': 2.0},
-    {'student_id': 5, 'course_code': 'CS102', 'grade': 1.0},
-    {'student_id': 5, 'course_code': 'CS103', 'grade': 5.0},
-    {'student_id': 5, 'course_code': 'CS104', 'grade': 4.0},
-    {'student_id': 5, 'course_code': 'CS105', 'grade': 3.0}
-    ]
+    def populate_sample_data(self):
+        # Connect to database
+        conn = sqlite3.connect(self.database)
+        c = conn.cursor()
+
+        # Execute SQL commands
+        c.executescript(self.sql_command)
+
+        # Commit and close connection
+        conn.commit()
+        conn.close()
+
+
+if __name__ == '__main__':
+    populate = Populate()
+    populate.populate_sample_data()
+    print('Sample data populated successfully')
+    print('Database: ' + populate.database)
+    print('SQL commands: ' + populate.sql_command)
+    print('Done')
+    exit(0)
